2feb7db81f113c7fd8f0b35ea0c7d2f2144e880c
Amit Levi <amtlvi101>
2024-03-04 00:37:22 +0200

    Editor POC

--- src/seltran/gui.py
+++ src/seltran/gui.py
@@ -42,22 +42,26 @@ class Editor(ctk.CTkFrame):
         self.textbox = ctk.CTkTextbox(master=self)
         self.textbox.tag_config(TAG_TRANSLATABLE, background="blue")
         self.textbox.tag_bind(
-            TAG_TRANSLATABLE, "<Button-1>", self.select_clicked_translatable
+            TAG_TRANSLATABLE, "<Button-1>", self.select_clicked_translatable_tag
         )
-        self.textbox.tag_config(TAG_SELECTED, background="yellow")
+        self.textbox.tag_config(TAG_SELECTED, background="red")
 
         self.translate_button = ctk.CTkButton(
             master=self,
             text="Translate",
-            command=self.refresh_nlp,
+            command=self.run_nlp,
         )
 
-        self.select_translation_combo = ctk.CTkComboBox(master=self)
+        self.select_translation_combo = ctk.CTkComboBox(
+            master=self, command=self.apply_picked_translation_to_selected
+        )
         self.reset_possible_translations()
 
-        self.textbox.pack()
-        self.select_translation_combo.pack()
-        self.translate_button.pack()
+        self.textbox.grid(sticky="EWNS")
+        self.select_translation_combo.grid(sticky="EWNS")
+        self.translate_button.grid(sticky="EWNS")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
     def _get_free_unique_tag(self) -> str:
         while True:
@@ -66,7 +70,7 @@ class Editor(ctk.CTkFrame):
             if tag not in self.unique_tags:
                 return tag
 
-    def refresh_nlp(self):
+    def run_nlp(self):
         # Clean up previous analysis
         self.unique_tags = dict()
         self.textbox.tag_remove(TAG_SELECTED, "1.0", "end")
@@ -93,8 +97,10 @@ class Editor(ctk.CTkFrame):
 
             self.textbox.tag_add(TAG_TRANSLATABLE, start, end)
             self.textbox.tag_add(unique_tag, start, end)
+        
+        # TODO: add whitespace after translatable tag to prevent merging tag ranges
 
-    def get_event_unique_tag(self, event) -> Optional[tuple[str, tuple[str, str]]]:
+    def get_unique_tag_for_event(self, event) -> Optional[tuple[str, tuple[str, str]]]:
         """Find the unique tag which contains an event's location, if there is one.
 
         :param event: Tk event
@@ -103,13 +109,20 @@ class Editor(ctk.CTkFrame):
         for tag in self.textbox.tag_names():
             if not is_unique_tag(tag):
                 continue
-            tag_ranges = self.get_event_ranges_of_tag(event, tag)
+            tag_ranges = self.get_tag_ranges_for_event(event, tag)
             if tag_ranges:
                 # Return first tag range as unique tags are assumed to only appear once
                 return (tag, tag_ranges[0])
         return None
 
-    def get_event_ranges_of_tag(self, event, tag: str) -> list[tuple[str, str]]:
+    def get_tag_ranges(self, tag: str) -> list[tuple[str, str]]:
+        tag_ranges = self.textbox.tag_ranges(tag)
+        return [
+            (str(tag_ranges[i]), str(tag_ranges[i + 1]))
+            for i in range(0, len(tag_ranges), 2)
+        ]
+
+    def get_tag_ranges_for_event(self, event, tag: str) -> list[tuple[str, str]]:
         """Find all index ranges of a specific tag matching an event's location
 
         :param event: Tk event
@@ -117,12 +130,7 @@ class Editor(ctk.CTkFrame):
         :return list[tuple[str, str]]: All matching index ranges which are tagged as `tag` and contain the event location
         """
         event_index = self.textbox.index(f"@{event.x},{event.y}")
-
-        tag_ranges = self.textbox.tag_ranges(tag)
-        tag_ranges = [
-            (str(tag_ranges[i]), str(tag_ranges[i + 1]))
-            for i in range(0, len(tag_ranges), 2)
-        ]
+        tag_ranges = self.get_tag_ranges(tag)
 
         def in_range_filter(tag_range: tuple[str, str]) -> bool:
             return is_index_in_range(
@@ -135,21 +143,33 @@ class Editor(ctk.CTkFrame):
         tags = []
 
         for tag in self.textbox.tag_names():
-            tag_ranges = self.textbox.tag_ranges(tag)
-            tag_ranges = [
-                (str(tag_ranges[i]), str(tag_ranges[i + 1]))
-                for i in range(0, len(tag_ranges), 2)
-            ]
-
+            tag_ranges = self.get_tag_ranges(tag)
             if tag_range in tag_ranges:
                 tags.append(tag)
 
         return tags
 
-    def select_clicked_translatable(self, event):
+    def get_unique_tag_of_range(self, tag_range: tuple[str, str]) -> Optional[str]:
+        for tag in self.get_tags_of_range(tag_range):
+            if is_unique_tag(tag):
+                return tag
+        return None
+
+    def get_selected(self) -> Optional[tuple[str, tuple[str, str]]]:
+        selected_ranges = self.get_tag_ranges(TAG_SELECTED)
+        if not selected_ranges:
+            return None
+        selected_range = selected_ranges[0]
+
+        selected_unique_tag = self.get_unique_tag_of_range(selected_range)
+        assert selected_unique_tag is not None
+
+        return (selected_unique_tag, selected_range)
+
+    def select_clicked_translatable_tag(self, event):
         # A unique tag is assured to be under the event since this callback is only
         # called for clicks on tagged text.
-        _ = self.get_event_unique_tag(event)
+        _ = self.get_unique_tag_for_event(event)
         assert _ is not None
         unique_tag, clicked_range = _
 
@@ -168,15 +188,30 @@ class Editor(ctk.CTkFrame):
         self.select_translation_combo.configure(values=[])
         self.select_translation_combo.set("No word selected")
 
+    def apply_picked_translation_to_selected(self, translation: str):
+        _ = self.get_selected()
+        assert _ is not None
+        _, selected_range = _
+        old_tags = self.get_tags_of_range(selected_range)
+
+        # Deleting the selected range also removes the selected and unique tags
+        self.textbox.delete(selected_range[0], selected_range[1])
+
+        self.textbox.insert(selected_range[0], translation)
+        selected_range = (selected_range[0], f"{selected_range[0]}+{len(translation)}c")
+        print(old_tags)
+        for tag in old_tags:
+            self.textbox.tag_add(tag, selected_range[0], selected_range[1])
+
 
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.editor = Editor(master=self)
-        self.editor.grid(sticky=tk.E + tk.W + tk.N + tk.S)
-
-        self.editor.pack()
+        self.editor.grid(sticky="EWNS")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
 
 def main():
